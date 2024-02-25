d5be88e9f5dfa93369d21a59fc9f408c4c49918e
Anakor Joshua <backend.mern@gmail.com>
2024-02-25 07:42:16 +0100

    Fix syntax bug in the redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,7 +3,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from '../features/posts/postsSlice';
 
 export const store = configureStore({
-  reducer: () => ({
+  reducer: {
     posts: postsReducer,
-  }),
+  },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
